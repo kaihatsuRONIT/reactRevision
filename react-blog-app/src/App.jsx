adc41f20b828f5a67174f692a9458728450a3096
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-14 23:11:51 +0530

    components

--- react-blog-app/src/App.jsx
+++ react-blog-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 import Header from "./Header";
 
 function App() {
@@ -6,10 +6,15 @@ function App() {
   return (
     <>
       <Header />
+      <Comp/>
       <h1>Hello world</h1>
       <h2>Counter : {counter}</h2>
       <button onClick={()=> setCounter(counter+1)}>increase counter</button> 
     </>
   )
+}
+
+function Comp(){
+  return 10*10+10*20 //it is a component as it start with capital letter and can return anything (values or html code)
 }
 export default App;  //name can be anything as it sends with DEFAULT
