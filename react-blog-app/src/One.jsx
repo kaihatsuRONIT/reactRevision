8cc17ddf684685777ab675c4fcafba4819253124
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-18 13:32:00 +0530

    functions can be passes through props

--- react-blog-app/src/One.jsx
+++ react-blog-app/src/One.jsx
@@ -1,9 +1,9 @@
-function One(props){ //recieve props by just typing without any destructuring; if destructured, we can extract and get direct individual elements
-    console.log(props)
+function One({sayName}){ //recieve props by just typing without any destructuring; if destructured, we can extract and get direct individual elements
+    // console.log(props)
     return(
     <>
-    <h1>{props.fruits[0]}</h1> {/* access with props. or through direct destructuring*/}
-    {/* <h1>{()=>props.sayName("ronit")}</h1> will not work as functions cannot be passed as react children */}
+    <h1>{props.fruits[0]}</h1> {/*access with props. or through direct destructuring*/}
+    <h1>{sayName("ronit")}</h1>
     </>
     )
 }
