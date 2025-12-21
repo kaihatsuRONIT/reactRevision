97ed70b9b092bdf7185dd830c44073e071f48d26
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-21 23:58:41 +0530

    loops with jsx

---
+++ react-blog-app/src/Tables.jsx
@@ -0,0 +1,103 @@
+function Tables(){
+    const userData = [
+        {
+            id:"1",
+            name:"rk",
+            email:"rk@google.com",
+            isLoggedIn:true
+        },
+        {
+            id:"2",
+            name:"sk",
+            email:"sk@google.com",
+            isLoggedIn:true
+        },
+        {
+            id:"3",
+            name:"ak",
+            email:"ak@google.com",
+            isLoggedIn:false
+        },
+        {
+            id:"4",
+            name:"github",
+            email:"gt@google.com",
+            isLoggedIn:false
+        }
+    ]
+    return (
+        <>
+        <div>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>IsLoggedIn</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        userData.map((user)=> (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.isLoggedIn}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <br />
+            <hr />
+            <br />
+            <h1>created manually </h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>Name</td>
+                        <td>Emaik</td>
+                        <td>IsLoggedIn</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>rk</td>
+                        <td>rk@google.com</td>
+                        <td>true</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>2</td>
+                        <td>github</td>
+                        <td>github@google.com</td>
+                        <td>false</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>3</td>
+                        <td>fb</td>
+                        <td>fb@google.com</td>
+                        <td>false</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>4</td>
+                        <td>sk</td>
+                        <td>sk@google.com</td>
+                        <td>true</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
+    )
+}
+export default Tables;
