d8703082324f2c50f67fe430ce1f970de5c8a6d0
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-25 21:40:56 +0530

    loop over component with custom css

--- react-css/src/App.jsx
+++ react-css/src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import User from "./User";
 
 function App() {
   const users = [1, 2, 3, 4]
+  const [cardStyle,setCardStyle] = useState({
+        width: "200px",
+        border: "1px solid #828282",
+        boxShadow: "2px 2px 3px 4px #dedede",
+        margin:"10px"
+    });
+    const updateTheme = (bgColor,textColor)=>{
+        setCardStyle({...cardStyle,backgroundColor:bgColor,color:textColor})
+    }
   return (
     <>
       <h1>Inline css with ReactJs</h1>
+      <button onClick={()=> updateTheme("#2f2e2e","yellow")}>gray</button>
+      <button onClick={()=> updateTheme("white","black")}>default</button>
       <div style={{display:"flex", flexWrap:"wrap"}}>
         {
         users.map((item) => {
           return(
             <div key={item}>
-              <User />
+              <User cardStyle={cardStyle}/>
             </div>
           )
         })
--- react-css/src/User.jsx
+++ react-css/src/User.jsx
@@ -1,13 +1,10 @@
-function User() {
-    const style = {
-        width: "200px",
-        border: "1px solid #828282",
-        boxShadow: "2px 2px 3px 4px #dedede",
-        margin:"10px"
-    }
+import { useState } from "react";
+
+function User({cardStyle}) {
+    
     return (
         <div style={{display:"flex"}}>
-            <div style={style}>
+            <div style={cardStyle}>
                 <img style={{ width: "200px" }} src="https://imgs.search.brave.com/8qbq9LFdiErbLA2_1SW-p1ExwNsF7W4tZIzCeXvTbJc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC8w/Ni8wNS9ibGluZGlu/Zy1za3VsbC1udW4t/d2l0aC1yYW5kb20t/Y29sb3JlZC12ZWN0/b3ItMzYwMTA2MDUu/anBn"
                     alt="photo.png"
                 />
