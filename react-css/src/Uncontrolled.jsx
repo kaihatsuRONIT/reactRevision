bfa01c5ebb1445bc7303354f95e6cad26bbbe483
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-26 23:42:29 +0530

    details on uncontrolled components

--- react-css/src/Uncontrolled.jsx
+++ react-css/src/Uncontrolled.jsx
@@ -1,18 +1,37 @@
 import { useRef, useState } from "react";
 
-function Uncontrolled(){
+function Uncontrolled() {
     const inputRef = useRef(null)
-    const h1Ref = useRef(null)
-    function inputRefHandler(){
+    const h3Ref = useRef(null)
+    function inputRefHandler(e) {
+        e.preventDefault();
         inputRef.current.focus()
         inputRef.current.style.color = "red"
-        h1Ref.current.style.color = "blue"
+        h3Ref.current.style.color = "blue"
+        console.log(inputRef.current.value)
+    }
+    function tempHandler() {
+        const elem = document.querySelector(".inpt")
+        document.querySelector(".ech3").style.color = "green"
+        elem.style.color = "red"
+        elem.focus()
     }
     return (
         <>
-        <input ref={inputRef} placeholder="Enter your name" type="text"/>
-        <h1 ref={h1Ref}>Temporary Project</h1>
-        <button onClick={inputRefHandler}>toggle inputRef </button>
+            <h1>Using useRef hook</h1>
+            <form action="" method="POST" onSubmit={(e) => inputRefHandler(e)}>
+                <h3 ref={h3Ref}>Temporary h3</h3>
+                <input ref={inputRef} type="text" placeholder="Enter your name" />
+                <button>submit</button>
+            </form>
+            <br />
+            <br />
+            <div>
+                <h1>Using querySelector or any method hook</h1>
+                <input className="inpt" placeholder="Enter your name" type="text" />
+                <h3 className="ech3">Temporary h3</h3>
+                <button onClick={tempHandler}>toggle changes </button>
+            </div>
         </>
     )
 }
