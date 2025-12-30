52e7b3f8d29cecf558a07f01d75b1576e5f3ded6
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-30 15:23:39 +0530

    updated arrays in react

--- react-blog-app/src/UpdateObjs.jsx
+++ react-blog-app/src/UpdateObjs.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 
 function UpdateObjs(){
+    const [index,setIndex] = useState("");
+    const [nameValue,setNameValue] = useState("");
+    const [dataDetails,setDataDetails] = useState([]);
     const [data,setData] = useState({
         name:"ronit",
         address:{
@@ -14,7 +17,12 @@ function UpdateObjs(){
     const handleCity = (val)=>{
         setData({...data,address:{...data.address,city:val}})
     }
-
+    const handleArrayValue = (val,idx)=>{
+        const newArr = [...dataDetails]
+        newArr[idx] = val;
+        setDataDetails(newArr);
+    }
+    console.log(dataDetails)
     return (
         <>
         <h3>update Objects</h3>
@@ -23,6 +31,18 @@ function UpdateObjs(){
         <p>Name : {data.name}</p>
         <p>City: {data.address.city}</p>
         <p>Country: {data.address.country}</p>
+        <br />
+        <br />
+        <hr />
+        <h3>update arrays</h3>
+        <input type="text" placeholder="Update name at given index" value={nameValue} onChange={(e)=>setNameValue(e.target.value)}/>
+        <input type="text" placeholder="enter index" value={index} onChange={(e)=>setIndex(e.target.value)}/>
+        <button onClick={()=>handleArrayValue(nameValue,index)}>set</button>
+        {
+            dataDetails.map((item,index)=>(
+                <p key={index}>{item}</p>
+            ))
+        }
         </>
     )
 }
