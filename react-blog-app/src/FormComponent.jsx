0a531f8c8f28e1b8c0390a63f1ff41723cf74f11
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-30 23:47:05 +0530

    added terms checkbox in FromComponent

--- react-blog-app/src/FormComponent.jsx
+++ react-blog-app/src/FormComponent.jsx
@@ -1,35 +1,47 @@
-import { useActionState } from "react"
+import { useActionState, useId, useState } from "react"
 
-function FormComponent(){
-    const handleFormAction = async (previousData,formData)=>{
-        await new Promise(res=>setTimeout(res,2000));
+function FormComponent() {
+    const handleFormAction = async (previousData, formData) => {
+        await new Promise(res => setTimeout(res, 2000));
         const name = formData.get("name")
         const pass = formData.get("password")
-        if(name && pass){
-            return {message:"Data Submitted"}
+        if (name && pass && isChecked) {
+            return { message: "Data Submitted", name, pass}
         }
-        else{
-            return {error:"All fields are required"}
+        else {
+            return { error: "All fields are required" }
         }
     }
-    const [data,action,pending] = useActionState(handleFormAction,undefined);
+    const [data, action, pending] = useActionState(handleFormAction, undefined);
+    const user = useId();
+    const [isChecked,setIsChecked] = useState(null);
+    const checkHandler = (e)=>{
+        setIsChecked(e.target.checked);
+    }
     return (
         <>
-        <div>
             <form action={action}>
-                <input type="text" placeholder="Enter your name" name="name" />
+                <label htmlFor={user + "name"}>Enter Name: </label>
+                <input id={user + "name"} type="text" placeholder="Enter your name" name="name" />
                 <br />
-                <input type="password"  placeholder="Enter your password" name="password"/>
+                <label htmlFor={user + "password"}>Enter Password: </label>
+                <input id={user + "password"} type="password" placeholder="Enter your password" name="password" />
                 <br />
-                <button disabled={pending}>{pending ? "submitting..." : "submit"}</button>
+                <label htmlFor={user + "terms"}>I Agree</label>
+                <input onChange={(e)=>checkHandler(e)} id={user + "terms"} type="checkbox" name="agreeCheck"/>
+                <br />
+                <button disabled={!isChecked || pending}>{pending ? "submitting..." : "submit"}</button>
             </form>
             {
-                data && <h3 style={{color:"green"}}>{data?.message}</h3>
+            data?.message && <>
+                <h3 style={{ color: "green" }}>{data?.message}</h3>
+                <p>Name : {data?.name}</p>
+                <p>PassWord : {data?.pass}</p>
+            </>
             }
             {
-                data && <h3 style={{color:"red"}}>{data?.error}</h3>
+                data?.error && <h3 style={{ color: "red" }}>{data?.error}</h3>
             }
-        </div>
         </>
     )
 }
