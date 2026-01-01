a9264e2e6ce825848e4fe944c57716a1d6942e9e
kaihatsuRONIT <roniitkhanna@gmail.com>
2026-01-02 00:04:30 +0530

    added subjects list in college.jsx

--- react-blog-app/src/Context_API/College.jsx
+++ react-blog-app/src/Context_API/College.jsx
@@ -1,15 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ClassComponent from "./ClassComponent";
 import { SubjectContext } from "./ContextData";
 
-function College(){
+function College() {
+    const [subject, setSubject] = useState("")
     return (
-        <div style={{backgroundColor: "yellow", padding:10}}>
-            <SubjectContext.Provider value="Science">
-                <h1>College</h1>
-                <ClassComponent/>
-            </SubjectContext.Provider>
-        </div>
+        <>
+            <div style={{ backgroundColor: "yellow", padding: 10 }}>
+                <SubjectContext.Provider value={subject}>
+                    <select value={subject} onChange={(e) => setSubject(e.target.value)}>
+                        <option value="">Select a value</option>
+                        <option value="English">English</option>
+                        <option value="Maths">Maths</option>
+                        <option value="Polity">Polity</option>
+                        <option value="History">History</option>
+                        <option value="Science">Science</option>
+                    </select>
+                    <h1>College</h1>
+                    <ClassComponent />
+                </SubjectContext.Provider>
+            </div>
+            <button onClick={() => setSubject("")}>clear</button>
+        </>
     )
 }
 export default College;
