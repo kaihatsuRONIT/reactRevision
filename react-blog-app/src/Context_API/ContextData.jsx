f7d4cc64b6365ca7b5f31eecc68254d737ed7aff
kaihatsuRONIT <roniitkhanna@gmail.com>
2026-01-02 23:51:23 +0530

    added scoreContext

--- react-blog-app/src/Context_API/ContextData.jsx
+++ react-blog-app/src/Context_API/ContextData.jsx
@@ -1,3 +1,4 @@
 import { createContext } from "react";
 
 export const SubjectContext = createContext("English");
+export const ScoreContext = createContext(97)
--- react-blog-app/src/Context_API/Subjects.jsx
+++ react-blog-app/src/Context_API/Subjects.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react"
-import { SubjectContext } from "./ContextData"
+import { ScoreContext, SubjectContext } from "./ContextData"
 
 function Subjects(){
-    const subject = useContext(SubjectContext)
+    const subject = useContext(SubjectContext);
+    const score = useContext(ScoreContext);
     return (
         <div style={{backgroundColor:"brown",padding:10}}>
         <h1 >Subjects : {subject}</h1>
+        <h2>Target Score : {score}</h2>
         </div>
     )
 }
