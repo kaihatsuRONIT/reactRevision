e442977ae551df336037fcc52538e6b38ddf642f
kaihatsuRONIT <roniitkhanna@gmail.com>
2026-01-02 23:51:50 +0530

    input field about score context

--- react-blog-app/src/Context_API/College.jsx
+++ react-blog-app/src/Context_API/College.jsx
@@ -1,26 +1,35 @@
 import { useContext, useState } from "react";
 import ClassComponent from "./ClassComponent";
-import { SubjectContext } from "./ContextData";
+import { ScoreContext, SubjectContext } from "./ContextData";
 
 function College() {
     const [subject, setSubject] = useState("")
+    const [score, setScore] = useState(0)
+
     return (
         <>
             <div style={{ backgroundColor: "yellow", padding: 10 }}>
                 <SubjectContext.Provider value={subject}>
-                    <select value={subject} onChange={(e) => setSubject(e.target.value)}>
-                        <option value="">Select a value</option>
-                        <option value="English">English</option>
-                        <option value="Maths">Maths</option>
-                        <option value="Polity">Polity</option>
-                        <option value="History">History</option>
-                        <option value="Science">Science</option>
-                    </select>
-                    <h1>College</h1>
-                    <ClassComponent />
+                    <ScoreContext.Provider value={score}>
+                        <select value={subject} onChange={(e) => setSubject(e.target.value)}>
+                            <option value="">Select a value</option>
+                            <option value="English">English</option>
+                            <option value="Maths">Maths</option>
+                            <option value="Polity">Polity</option>
+                            <option value="History">History</option>
+                            <option value="Science">Science</option>
+                        </select>
+                        <br />
+                        <input type="text" value={score} onChange={(e)=>setScore(e.target.value)}/>
+                        <h1>College</h1>
+                        <ClassComponent />
+                    </ScoreContext.Provider>
                 </SubjectContext.Provider>
             </div>
-            <button onClick={() => setSubject("")}>clear</button>
+            <button onClick={() => {
+                setSubject("")
+                setScore(0)
+            }}>clear</button>
         </>
     )
 }
