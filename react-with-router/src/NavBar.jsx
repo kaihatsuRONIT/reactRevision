5ac95478e9a1d1327fe58ee20bc0831315861b8b
kaihatsuRONIT <roniitkhanna@gmail.com>
2026-01-05 23:12:16 +0530

    difference b/w navLink and link and custom-active class

--- react-with-router/src/NavBar.jsx
+++ react-with-router/src/NavBar.jsx
@@ -1,20 +1,20 @@
-import { Link, Navigate, Outlet } from "react-router";
+import { Link, Navigate, NavLink, Outlet } from "react-router";
 import './header.css' 
 function NavBar() {
     return (
         <div>
             <div className="top">
             <div>
-                <Link className="link" to={"/"}><h2>Logo</h2></Link>
+                <NavLink className={({isActive}) => isActive ? "custom-active link" : " link" } to={"/"}><h2>Logo</h2></NavLink>
             </div>
             <div className="header">
                 <ul>
-                    <li ><Link to="/" className="link"><h2>Home</h2></Link></li>
-                    <li><Link to="/user/about" className="link"><h2>About</h2></Link></li>
-                    <li><Link to="/collections" className="link"><h2>Collections</h2></Link></li>
-                    <li><Link to="/contact" className="link"><h2>Contact Us</h2></Link></li>
-                    <li><Link to="/college" className="link"><h2>College</h2></Link></li>
-                    <li><Link to="/users" className="link"><h2>User</h2></Link></li>
+                    <li ><NavLink to="/" className="link"><h2>Home</h2></NavLink></li>
+                    <li><NavLink to="/user/about" className="link"><h2>About</h2></NavLink></li>
+                    <li><NavLink to="/collections" className="link"><h2>Collections</h2></NavLink></li>
+                    <li><NavLink to="/contact" className="link"><h2>Contact Us</h2></NavLink></li>
+                    <li><NavLink to="/college" className="link"><h2>College</h2></NavLink></li>
+                    <li><NavLink to="/users" className="link"><h2>User</h2></NavLink></li>
                 </ul>
             </div>
         </div>
--- react-with-router/src/Routes/UserList.jsx
+++ react-with-router/src/Routes/UserList.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router"
+import { Link } from "react-router"
 
 function UserList(){
     const userData = [
