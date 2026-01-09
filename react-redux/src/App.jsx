6086a9ead3edb0d341ef757369f62127efac36a1
kaihatsuRONIT <roniitkhanna@gmail.com>
2026-01-09 07:52:14 +0530

    how to pass values in reducer functions

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,8 +1,10 @@
 import {useDispatch, useSelector} from "react-redux"
-import { decrement, increment } from "./counterReducer";
+import { decrement, increment, incrementByAmount } from "./counterReducer";
+import { useState } from "react";
 function App(){
   const count = useSelector((state)=>state.counter.value);
   const dispatch = useDispatch()
+  const [amt,setAmt] = useState(0);
   return (
     <>
     <h1>{count}</h1>
@@ -14,6 +16,11 @@ function App(){
     <button onClick={()=>{
       dispatch(decrement())
     }}>decrement</button>
+
+    <input type="number" onChange={(e)=> setAmt(e.target.value)}/>
+    <button onClick={()=>{
+      dispatch(incrementByAmount(Number (amt)))
+    }}>increment by amount</button>
     </>
   )
 }
